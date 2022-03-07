059c4fcd26c96bf5694169fe83153e9d79240c01
mrizkiadi24 <91538874+mrizkiadi24@users.noreply.github.com>
2022-03-07 11:45:28 +0700

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+
+$servername     = "localhost";
+$database       = "searching";
+$username       = "root";
+$password       = "";
+
+// membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $database);
