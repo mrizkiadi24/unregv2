7df46c6709f2e97d27aeb5d13e0fffc7c59fb5a0
mrizkiadi24 <91538874+mrizkiadi24@users.noreply.github.com>
2022-03-07 11:46:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	//Only needed for the filename of export files.
+	//Normally set in the title tag of your page.
+	document.title='Simple DataTable';
+	// DataTable initialisation
+	$('#example').DataTable(
+		{
+			"dom": '<"dt-buttons"Bf><"clear">lirtp',
+			"paging": true,
+			"autoWidth": true,
+			"buttons": false
+		}
+	);
+});
